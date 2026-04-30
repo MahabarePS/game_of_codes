414b09b2ef344e68956391466e13877f70f7c849
MahabarePS <prasadmahabare1@gmail.com>
2026-04-30 22:53:14 +0530

    started brain storming at leaast

--- leetCode75/1071.c
+++ leetCode75/1071.c
@@ -1,15 +1,16 @@
 /*
- * author Saquib Shaikh
- * created on 18-04-2026-21h-07m
- * github: https://github.com/saquibshaikh14
+ * author Prasad Mahabare
+ * created on 30-04-2026-21h-56m
+ * github: https://github.com/MahabarePS
  * copyright 2026
 */
 /*
-For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+For two strings s and t, we say "t divides s" if and 
+only if s = t + t + t + ... + t + t 
+(i.e., t is concatenated with itself one or more times).
 
-Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
-
- 
+Given two strings str1 and str2, return the largest string x such that 
+x divides both str1 and str2.
 
 Example 1:
 
@@ -42,3 +43,9 @@ Constraints:
 1 <= str1.length, str2.length <= 1000
 str1 and str2 consist of English uppercase letters.
 */
+#include<stdio.h>
+
+char* gcdOfStrings(char*, char*);
+int main(){
+    
+}
