555e7bfb2b378779f28d69b688ea73ce4d7a4edd
MahabarePS <prasadmahabare1@gmail.com>
2026-06-19 15:54:11 +0530

    leetcode: 643.c

---
+++ leetCode75/slidingWindow/643.c
@@ -0,0 +1,31 @@
+/*
+ * author Prasad Mahabare
+ * created on 19-06-2026-15h-52m
+ * github: https://github.com/MahabarePS
+ * copyright 2026
+*/
+
+/*
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+ 
+
+Constraints:
+
+n == nums.length
+1 <= k <= n <= 105
+-104 <= nums[i] <= 104
+*/
