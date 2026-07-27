20e9dee65245ff6379c6f4ad1360b62cd0290434
MahabarePS <prasadmahabare1@gmail.com>
2026-07-27 21:23:23 +0530

    back to the basics: break

---
+++ fundamentals/break.c
@@ -0,0 +1,17 @@
+/*
+ * author Prasad Mahabare
+ * created on 27-07-2026-21h-18m
+ * github: https://github.com/MahabarePS
+ * copyright 2026
+*/
+
+#include<stdio.h>
+int main(){
+    int i;
+    for(i=0;i<10;i++){
+        if(i==4)
+        break;
+        printf("%d\n",i);
+    }
+    return 0;
+}
