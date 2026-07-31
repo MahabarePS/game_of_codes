2959b75166f3ccc2eedfb50f640e2b93fb8f6ac6
MahabarePS <prasadmahabare1@gmail.com>
2026-07-31 23:07:32 +0530

    creating a problem statement

---
+++ fundamentals/hashTables/hashTables.c
@@ -0,0 +1,18 @@
+/*
+ * author Prasad Mahabare
+ * created on 31-07-2026-22h-48m
+ * github: https://github.com/MahabarePS
+ * copyright 2026
+*/
+/*
+In Hash Table:
+1. searching adding and, deleting data is fast compared to Array and linkedlist.
+2. with hash tables you can directly access the element by its location using hash function.
+
+Creating a simple hash table:
+1. Starting with an array.
+2. Storing names using a hash function.
+3. Looking up an element using a hash function.
+4. Handling collisions.
+5. The basic hash set code example and simulation.
+*/
