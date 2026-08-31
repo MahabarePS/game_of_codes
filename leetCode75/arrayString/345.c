b93180ba447efa0541e7f0e9d9d1b559a527f0cc
MahabarePS <prasadmahabare1@gmail.com>
2026-08-31 20:59:42 +0530

    first iteration

--- leetCode75/arrayString/345.c
+++ leetCode75/arrayString/345.c
@@ -34,12 +34,30 @@ Constraints:
 s consist of printable ASCII characters.
 */
 #include<stdio.h>
+#define SIZE 100
 char* reverseVowels(char* s) {
-    
+    char temp[SIZE] = "";
+    int i=0;
+    //To store the vowels from the string in order
+    for(i=0;s[i]!=0;i++){
+        if((s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')||
+            (s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')){
+                temp[i]==s[i];
+            }
+    }
+    //need to reverse
+    size=i;
+    //reverse the order of the vowels
+    for(int i=size;i!=0;i--){
+        if((s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')||
+            (s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')){
+                s[i]==temp[i];
+            }
+    }
 }
 int main(){
-    char s = "IceCreAm";
-    char size = sizeof(s);
-    printf("Output: %s",reverseVowels(s));
+    char s[SIZE] = "IceCreAm";
+    reverseVowels(s);
+    printf("Output: %s\n",s);
     return 0;
 }
